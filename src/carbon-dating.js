41b8a27e9a53bcaf5c294a55bfd2d8240d5f30bd
Vadim Shatalov <vadimshatalov@gmail.com>
2020-02-17 04:06:20 +0400

    solve createDreamTeam

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,5 +2,6 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-
+  throw 'Not implemented';
+  // remove line with error and write your code here
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,11 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let result = [];
+
+  if ( Object.prototype.toString.call(members) != '[object Array]'){ return false; }
+  members.forEach( function(member) {
+    if (typeof(member) === 'string'){
+      result.push(member.trim()[0].toUpperCase())
+    }
+  });
+  return result ? result.sort().join("") : false
 };
