9926ad405dbe8e318aa1cb260a179c664e8008e7
Vadim Shatalov <vadimshatalov@gmail.com>
2020-02-17 23:08:48 +0400

    solve getSeason

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,21 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(input_date) {
+  if (!input_date) {
+    return 'Unable to determine the time of year!';
+  }
+  if ( Object.prototype.toString.call(input_date) != '[object Date]'){ throw Error; }
+
+  const month = input_date.getMonth();
+
+  const seasonRanges = {
+    'winter': [11, 0, 1],
+    'spring': [2, 3, 4],
+    'summer': [5, 6, 7],
+    'autumn': [8, 9, 10]
+  };
+
+  for (let key in seasonRanges) {
+    if (seasonRanges[key].includes(month)) {
+      return key
+    }
+  }
 };
